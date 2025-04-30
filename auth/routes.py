edcd7b0c759933d50d9574b0a35272b4f8ec88ce
Peter Papasotiriou <africalodge@hotmail.com>
2025-04-30 17:12:32 +0200

    Update routes.py

--- auth/routes.py
+++ auth/routes.py
@@ -7,7 +7,6 @@ from passlib.context import CryptContext
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from fastapi.security import OAuth2PasswordBearer
-from utils.recaptcha import verify_recaptcha
 import os
 from dotenv import load_dotenv
 load_dotenv()
