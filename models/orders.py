987c788977d5a83ec27103739c0b9a9d11acd6e1
Peter Papasotiriou <africalodge@hotmail.com>
2025-05-02 20:26:18 +0200

    b

--- models/orders.py
+++ models/orders.py
@@ -7,6 +7,7 @@ class Order(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    email = Column(String, nullable=False)
     product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     amount_paid = Column(Float, nullable=False)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
